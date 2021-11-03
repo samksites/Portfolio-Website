939efd10ae92470f676dac9069acf0ea1e601c53
samksites <32577678+samksites@users.noreply.github.com>
2021-11-03 01:12:29 -0400

    change to app.js

--- express/app.js
+++ express/app.js
@@ -1,14 +1,7 @@
 const express = require('express');
 
-const app = express()
+const app = express();
 const port = 3000;
-
-let i = 0;
-
-app.get('/tick', (request, response) => {
-    i += 1;
-    response.send({number: i})
-})
 
 app.listen(port, () => {
     console.log(`We're live on port ${port}!`)
