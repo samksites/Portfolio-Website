7ce4644053850745d471af55383a37a9766cff83
samksites <32577678+samksites@users.noreply.github.com>
2022-05-22 14:27:07 -0400

    about me section need photos

--- react_web/src/componnents/personalInfo/photoFrames.js
+++ react_web/src/componnents/personalInfo/photoFrames.js
@@ -6,7 +6,7 @@ import jmu from './assets/jmu.jpg'
 const PhotoFrames = (props) => {
    
     return (
-        <img id="portrait" style={{height: props.height, width: props.width}} src={jmu} alt='Temp' ></img>
+        <img id="portrait" style={{height: props.height, width: props.width, transform: "rotateZ(" + props.rotate +")"}} src={jmu} alt='Temp' ></img>
     )
 }
 
